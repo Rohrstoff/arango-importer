06a9d44f0202301175041df2f0887b6f77a30d66
Nico <nicolas.rohr@fhnw.ch>
2020-05-28 20:01:01 +0200

    Added AltoRouter + changed from httpd to nginx

--- index.php
+++ index.php
@@ -6,9 +6,32 @@ use App\Database\ArangoConnection;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+$router = new AltoRouter();
 $loader = new FilesystemLoader( 'views' );
 $twig = new Environment($loader);
 
 $con = new ArangoConnection();
 
-echo $twig->render( 'index.html', ['message' => $con->helloWorld()] );
+$router->map('GET', '/', function () use ($twig, $con)
+{
+	echo $twig->render( 'index.html', ['message' => $con->helloWorld()] );
+});
+
+$router->map('GET', '/hello-world', function () use ($twig, $con)
+{
+	echo 'Hello World!';
+});
+
+// match current request url
+$match = $router->match();
+
+// call closure or throw 404 status
+if ( is_array($match) && is_callable( $match['target'] ) )
+{
+	call_user_func_array( $match['target'], $match['params'] );
+}
+else
+{
+	// no route was matched
+	header( $_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+}
